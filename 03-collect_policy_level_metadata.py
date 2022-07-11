9aeb3e1bf1fe256349e6f55458fa87a9d8aa74e9
Benjamin Mako Hill <mako@atdot.cc>
2022-07-11 15:22:31 +0200

    added data with timestamps and order within/across wikis

--- 03-collect_policy_level_metadata.py
+++ 03-collect_policy_level_metadata.py
@@ -6,6 +6,7 @@ import numpy as np
 import datetime
 import re
 import urllib.parse
+from datetime import datetime 
 
 ## this is the only line you'll need to change to get this to work in 
 sitelink_df=pd.read_csv('generated_data/policy_page_links.csv')
@@ -14,16 +15,68 @@ sitelink_df=pd.read_csv('generated_data/policy_page_links.csv')
 # get the page name
 sitelink_df['title'] = sitelink_df.url.apply(lambda x: urllib.parse.unquote(re.sub('^.*/wiki/', '', x)))
 
-sitelink_df['base_api'] = 'https://' + sitelink_df.lang + '.wikipedia.org/w/api.php?titles=' + sitelink_df.title + '&format=json'
+timestamp_list = []
+for row in sitelink_df[['lang', 'title']].iterrows():
+    lang = row[1]['lang']
+    title = row[1]['title']
 
-timestamp_list=[]
-for base_api in sitelink_df['base_api']:
-    api_link = base_api + '&action=query&prop=revisions&rvlimit=1&rvprop=timestamp&rvdir=newer'
-    r = requests.get(api_link)
+    params = {'titles' : title,
+              'format' : 'json',
+              'action' : 'query',
+              'prop' : 'revisions',
+              'rvlimit' : 1,
+              'rvprop' : 'timestamp',
+              'rvdir' : 'newer' }
+
+    base_url = 'https://' + lang + '.wikipedia.org/w/api.php'
+   
+    # gather the data
+    r = requests.get(base_url, params=params)
     data = r.json()
-    if '-1' in data['query']['pages']: ## page is missing
-        timestamp = timestamp_list.append('NA')
-    else:
-        timestamp=list(data['query']['pages'].values())[0]['revisions'][0]['timestamp']
-    timestamp_list.append(timestamp)
 
+    print(lang, title)
+    if len(data['query']['pages']) != 1:
+        print("ERROR: more than one page id returned")
+        break
+
+    # iterate through each pageid
+    for pageid in data['query']['pages']:
+        page_info = data['query']['pages'][pageid]
+        if 'missing' in page_info:
+            timestamp = 'NA'
+        else:
+            timestamp = page_info['revisions'][0]['timestamp']
+
+        timestamp_list.append(timestamp)
+
+## append the timestamp
+sitelink_df['timestamp'] = timestamp_list
+
+# drop the NA which is a missing page
+sitelink_df = sitelink_df[sitelink_df['timestamp'] != "NA"]
+
+## create order within wiki
+df_new = pd.DataFrame()
+for lang, df_subset in sitelink_df.groupby('lang'):
+    df_subset = df_subset.sort_values(by="timestamp")
+    df_subset["order_within_wiki"] = range(0, df_subset.shape[0])
+    df_new = df_new.append(df_subset)
+sitelink_df = df_new
+
+## create order across wiki
+df_new = pd.DataFrame()
+for qid, df_subset in sitelink_df.groupby('QID'):
+    df_subset = df_subset.sort_values(by="timestamp")
+    df_subset["order_across_wikis"] = range(0, df_subset.shape[0])
+    df_new = df_new.append(df_subset)
+sitelink_df = df_new
+
+# write out the post03 data processing
+sitelink_df.to_csv('generated_data/policy_page_links-post03.csv', index=False)
+
+# old code to split groups intos groups of 50 titles by wiki
+# title_max = 1
+# for lang, df_subset in sitelink_df.groupby('lang'):
+#     title_batches = [df_subset['title'][i:i+title_max] for i in range(0, df_subset.shape[0], title_max)]
+#     for batch in title_batches:
+#         title_slug = "|".join([x for x in batch])
